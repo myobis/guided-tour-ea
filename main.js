f19eb0a1c56ced988bc8b3164f874927ce3de80c
Olivier Pessin <opessin@kds.com>
2021-08-27 00:28:28 +0200

    Added KML layer

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import Stamen from 'ol/source/Stamen';
+import KML from 'ol/format/KML';
+import {fromLonLat} from 'ol/proj';
+import VectorSource from 'ol/source/Vector';
+import {Vector as VectorLayer} from 'ol/layer';
+
+
+let kmlLayer = new VectorLayer({
+  source: new VectorSource({
+    url: './TestLiergues.kml',
+    format: new KML()
+  })
+});
 
 new Map({
   target: 'map',
@@ -8,12 +20,14 @@ new Map({
     new TileLayer({
       source: new Stamen({layer: 'watercolor'}),
     }),
+    //new TileLayer({ source: new Stamen({layer: 'terrain-lines'}), }),
+    kmlLayer,
     new TileLayer({
       source: new Stamen({layer: 'toner-labels'}),
     }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([4.663223,45.9704547]),
+    zoom: 15
   })
 });
